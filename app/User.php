680a4373aad9d7b8062117ecf177c621500cb571
Ants <antssanjeewa94@gmail.com>
2020-04-28 15:16:34 +0530

    add project

---
+++ app/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable, HasApiTokens,SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'address','lat','long','phoneNo','whatsappNo','role','password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function findForPassport($username)
+    {
+        return $this->where('phoneNo', $username)->first();
+    }
+
+    public function vegetables(){
+        return $this->hasMany('App\Model\Vegetable','farmerId');
+    }
+}
