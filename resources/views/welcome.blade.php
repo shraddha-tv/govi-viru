e9dc4e0824e7b92cbd944c0db0c0e6b0a2cce919
shraddha-tv <web@shraddha.lk>
2020-05-03 00:35:30 +0530

    test 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,7 +149,8 @@
                 let token = await creds.user.getIdToken()
                 console.log({ token })
                 let headers = { Authorization: 'Bearer ' + token }
-                let me = await axios.get('/api/phone?phoneNo=0712576076', { headers })
+                let me = await axios.get('/api/me', { headers })
+                // let me = await axios.get('/api/phone?phoneNo=0712576076', { headers })
                 console.log({ me })
 
 
